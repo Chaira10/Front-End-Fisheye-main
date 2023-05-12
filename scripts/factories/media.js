ca18e9e84f5c7ee6f6cf8db9e9708f7e40b33649
chaira <chaira10@hotmail.fr>
2023-05-12 03:04:48 +0200

    ajout de la card au survol img

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,103 +1,105 @@
 
+function mediaFactory(data) {
+  const mediaTypes = {
+    image: 'img',
+    video: 'video'
+  };
 
-    function mediaFactory(data) {
-        const { id, photographerId, title, image, likes, date, price } = data;
-    
-        const picture = `assets/photographers/${photographerId}/${image}`;
-        const video = `assets/photographers/${photographerId}/${title}`;
+  const { id, photographerId, title, image, likes, date, price, video } = data;
 
-    
-        function getMediaCardDOM() {
-            // creation de l'élément article
-            const article = document.createElement( 'article' );
-            article.classList.add('media-card', 'card-media'); // Ajout d'une classe à l'élément <article>
-            if (video) {
-                fetch(video)
-                    .then(response => {
-                        if (response.ok) {
-            // creation de la div video
-            const divVideo = document.createElement( 'video' );
-            divVideo.setAttribute("controls", '');
+  const mediaType = video ? 'video' : 'image';
+  const mediaPath = `assets/photographers/${photographerId}/${mediaType === 'image' ? image : video}`;
 
-            divVideo.classList.add('media-video');
-   
+  // Création de la div hover-info
+  const hoverDiv = document.createElement('div');
+  hoverDiv.classList.add('hover');
 
-            // creation de la balise video
-            const baliseVideo = document.createElement( 'source' );
-            baliseVideo.setAttribute("src", video);
-            baliseVideo.setAttribute("type", 'video/mp4');
-            baliseVideo.width = '323'; // Définition de la largeur de la vidéo
-            baliseVideo.height = '323'; // Définition de la hauteur de la vidéo
-            divVideo.appendChild( baliseVideo );
-            article.appendChild(divVideo);
-        }
-    });
-}
+  const spanHover = document.createElement('span');
+  spanHover.classList.add('hover-info');
+  const likesSpan = document.createElement('span');
+  likesSpan.textContent = likes;
+  likesSpan.classList.add('hover-likes');
 
-if (picture) {
-    fetch(picture)
-        .then(response => {
-            if (response.ok) {
-            // creation div img
-            const divImg = document.createElement('div');
-            divImg.classList.add('img-media', 'media-img');
-            // creation de la balise img
-            const img = document.createElement( 'img' );
-            img.classList.add('card-media', 'card-media'); // Ajout d'une classe à l'élément <img>
-            // ajout de l'attribut scr + chemin
-            img.setAttribute("src", picture);
-            img.setAttribute('alt', title); // Ajout de l'attribut "alt" avec la même valeur que "name"
-            img.setAttribute('data-id', id); // Ajout de l'attribut data-id 
-            img.setAttribute('id', 'img'); // Ajout de l'attribut id 
-            //ajout de l'image dans la balise article
-            divImg.appendChild(img);
-            article.appendChild(divImg);
-        }
-    });
-}
+  const likesIcon = document.createElement('i');
+  likesIcon.classList.add('fa', 'fa-heart');
+  likesSpan.appendChild(likesIcon);
 
-    
-            // creation de la div text
-            const div = document.createElement('span');
-            div.classList.add('text-media', 'media-text');
-            // creation de l'élément h2
-            const h2 = document.createElement( 'h2' );
-            h2.classList.add('card-title', 'title-card'); // Ajout d'une classe à l'élément <h2>
-            // ajout du nom dans le h2
-            h2.textContent = title;
-            // ajout du h2 dans la div
-            div.appendChild(h2);
-            //ajout du nom dans la balise article
-    
-            // article.appendChild(h2);
-    
-            // creation de la balise span
-            const span1 = document.createElement( 'span' );
-            span1.classList.add('card-likes', 'likes-card'); // Ajout d'une classe à l'élément <span>
-            // ajout de la ville et du pays dans le span
-            span1.textContent = likes;
-            // création de l'élément i pour l'icône de coeur
-            const heartIcon = document.createElement('i');
-            heartIcon.classList.add('fa', 'fa-sharp', 'fa-solid', 'fa-heart');
+  // const dateSpan = document.createElement('span');
+  // dateSpan.textContent = date;
+  // dateSpan.classList.add('hover-date');
 
-// ajout de l'icône de coeur à l'élément span1
-span1.appendChild(heartIcon);
-            // ajout du span dans la balise article
-            // article.appendChild( span1 );
-            div.appendChild( span1 );
-    
-            // creation de la balise p
-            const p2 = document.createElement( 'p' );
-            p2.classList.add('media-price', 'price-media'); // Ajout d'une classe à l'élément <p>
-            // ajout du prix dans le p
-            p2.textContent = ` ${price}$ / jour`;
-            // ajout du p dans la balise article
-            article.appendChild( p2 );
-            // div.appendChild( p2 );
+  const priceSpan = document.createElement('span');
+  priceSpan.textContent = `${price}$ / jour`;
+  priceSpan.classList.add('hover-price');
 
-            article.appendChild( div );
-    
-            return (article);
-        }
-        return { id, photographerId, title, image, likes, date, price , picture, getMediaCardDOM }
+  spanHover.appendChild(likesSpan);
+  // hoverDiv.appendChild(dateSpan);
+  spanHover.appendChild(priceSpan);
+  hoverDiv.appendChild(spanHover);
+
+  function getMediaCardDOM() {
+    const article = document.createElement('article');
+    article.classList.add('media-card', 'card-media');
+
+    const media = document.createElement(mediaTypes[mediaType]);
+    media.classList.add('media');
+    media.setAttribute('alt', title);
+    media.setAttribute('src', mediaPath);
+
+    if (mediaType === 'video') {
+      media.setAttribute('controls', '');
+      media.setAttribute('type', 'video/mp4');
+    } else {
+      media.setAttribute('type', 'image/jpg');
     }
+
+    media.setAttribute('width', '500');
+    media.setAttribute('height', '500');
+
+    // Ajout de la div hover-info à l'article
+    article.appendChild(hoverDiv);
+
+    // Masquage de la div hover-info par défaut
+    hoverDiv.style.display = 'none';
+
+    // Affichage de la div hover-info au survol
+    media.addEventListener('mouseover', () => {
+      hoverDiv.style.display = 'block';
+    });
+
+    // Masquage de la div hover-info à la fin du survol
+    media.addEventListener('mouseout', () => {
+      hoverDiv.style.display = 'none';
+    });
+
+    article.appendChild(media);
+
+    const div = document.createElement('div');
+    div.classList.add('text-media', 'media-text');
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('card-title', 'title-card');
+    h2.textContent = title;
+
+    const span1 = document.createElement('span');
+    span1.classList.add('card-likes', 'likes-card');
+    span1.textContent = likes;
+
+    const heartIcon = document.createElement('i');
+    heartIcon.classList.add('fa', 'fa-sharp', 'fa-solid', 'fa-heart');
+    span1.appendChild(heartIcon);
+
+    const p2 = document.createElement('p');
+    p2.classList.add('media-price', 'price-media');
+    p2.textContent = ` ${price}$ / jour`;
+
+    div.appendChild(h2);
+    div.appendChild(span1);
+    div.appendChild(p2);
+    article.appendChild(div);
+
+    return article;
+  }
+
+  return { id, photographerId, title, image, likes, date, price, mediaType, mediaPath, getMediaCardDOM }
+}
